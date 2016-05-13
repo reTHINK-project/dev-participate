3a737268c9948f0a995fb1a60e1f97e4af3ffbf0
David Vílchez <dvilchez@xuaps.com>
2016-05-13 09:17:00 +0200

    Add fake participants hyperty

---
+++ resources/ParticipantsHyperty.js
@@ -0,0 +1,32 @@
+import HypertyDiscovery from 'service-framework/src/hyperty-discovery/HypertyDiscovery'
+import URI from 'urijs'
+
+const ParticipantsHyperty = {
+    getParticipants(){
+        let users = [
+            'openidtest10@gmail.com',
+            'openidtest20@gmail.com',
+            'openidtest30@gmail.com'
+        ]
+
+        return Promise.all(users.map((p)=>{
+            return ParticipantsHyperty.hypertyDiscovery.discoverHypertyPerUser(p, ParticipantsHyperty.domain)
+                .then((user)=>{ return { email: p, online: true } }) 
+                .catch((error)=>{ return { email: p, online: false } })
+        }))
+    }
+}
+
+const ParticipantsHypertyFactory = function(hypertyURL, bus, config){
+    ParticipantsHyperty.domain = new URI(hypertyURL).hostname()
+    ParticipantsHyperty.hypertyDiscovery = new HypertyDiscovery(hypertyURL, bus)
+
+    return ParticipantsHyperty
+}
+
+export default function activate(hypertyURL, bus, config){
+    return {
+        name: 'ParticipantsHyperty',
+        instance: ParticipantsHypertyFactory(hypertyURL, bus, config)
+    }
+}
