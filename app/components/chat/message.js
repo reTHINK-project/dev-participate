43b848dc4cf9dfd750b969ce882c9453cf29eb01
David Vílchez <dvilchez@xuaps.com>
2016-06-01 18:37:50 +0200

    Improve a bit the chat layout

--- app/components/chat/message.js
+++ app/components/chat/message.js
@@ -2,13 +2,19 @@ import React, { PropTypes } from 'react'
 
 const Message = ({content, time, isMe, identity}) => {
     return(
-            <li className={(isMe?"right":"left")+ "clearfix"}><span className={"chat-img pull-"+(isMe?"right":"left")}>
-                <img src={identity.avatar} alt="User Avatar" className="img-circle avatar"/>
-            </span>
+            <li className={(isMe?"right":"left")+ "clearfix"}>
+                <span className={"chat-img pull-"+(isMe?"right":"left")}>
+                    <img src={identity.avatar} alt="User Avatar" className="img-circle avatar"/>
+                </span>
                 <div className="chat-body clearfix">
-                    <div className="header">
-                        <strong className={(isMe?"":"pull-right")+" primary-font"}>{identity.username}</strong> <small className={(isMe?"":"pull-right")+ " text-muted"}>
-                            <span className="glyphicon glyphicon-time"></span>{Math.round((((Date.now()-time) % 86400000) % 3600000) / 60000)} mins ago</small>
+                    <div className={(isMe?"":"pull-right")+" header"}>
+                        <strong className="primary-font">
+                            {identity.username + "  "}
+                        </strong> 
+                        <small className="text-muted">
+                            <span className="glyphicon glyphicon-time"></span>
+                            {Math.round((((Date.now()-time) % 86400000) % 3600000) / 60000)} mins ago
+                        </small>
                     </div>
                     <p>
                         {content}
