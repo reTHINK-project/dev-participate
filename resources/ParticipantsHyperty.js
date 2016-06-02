d37bf62d132a17c8f8bea132ecc51a93e08c053e
David Vílchez <dvilchez@xuaps.com>
2016-06-02 19:02:43 +0200

    Remove actual user from participant list

--- resources/ParticipantsHyperty.js
+++ resources/ParticipantsHyperty.js
@@ -1,7 +1,25 @@
 import HypertyDiscovery from 'service-framework/src/hyperty-discovery/HypertyDiscovery'
+import IdentityManager from 'service-framework/dist/IdentityManager'
 import URI from 'urijs'
 
 const ParticipantsHyperty = {
+    _resolveIdentity(){
+        return new Promise((resolve, reject)=>{
+                if(this.identity)
+                {
+                    resolve(this.identity)
+                }
+                else
+                {
+                    this.identityManagerService.discoverUserRegistered('.', this.hypertyURL)
+                        .then((identity)=>{
+                            this.identity=identity
+                            resolve(identity)
+                        })
+                }
+            })
+    },
+
     getParticipants(){
         let users = [
             'openidtest10@gmail.com',
@@ -9,19 +27,22 @@ const ParticipantsHyperty = {
             'openidtest30@gmail.com'
         ]
 
-        return Promise.all(users.map((p)=>{
-            console.log("tachan")
-            console.log(this)
-            return ParticipantsHyperty.hypertyDiscovery.discoverHypertyPerUser(p, ParticipantsHyperty.domain)
-                .then((user)=>{ return { email: p, online: true } }) 
-                .catch((error)=>{ return { email: p, online: false } })
-        }))
+        let identity = undefined
+        return this._resolveIdentity()
+            .then((identity)=>Promise.all(users.filter((u)=>u!==identity.username).map((p)=>{
+                return ParticipantsHyperty.hypertyDiscovery.discoverHypertyPerUser(p, ParticipantsHyperty.domain)
+                    .then((user)=>{ return { email: p, online: true } }) 
+                    .catch((error)=>{ return { email: p, online: false } })
+            })))
     }
 }
 
 const ParticipantsHypertyFactory = function(hypertyURL, bus, config){
+    let identityManager = new IdentityManager(hypertyURL, config.runtimeURL, bus)
+
     ParticipantsHyperty.domain = new URI(hypertyURL).hostname()
     ParticipantsHyperty.hypertyDiscovery = new HypertyDiscovery(hypertyURL, bus)
+    ParticipantsHyperty.identityManagerService = identityManager
 
     return ParticipantsHyperty
 }
