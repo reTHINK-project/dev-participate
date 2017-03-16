e8d64509391ba641b6a4d5bb5a7ef22bb4a0f15a
David Vílchez <dvilchez@xuaps.com>
2017-03-16 11:59:22 +0100

    Add new group reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,11 @@
 const chatApp = (state, action) => {
-    if(action.type === 'ACTION_CREATED') {
-        return { ...state, actions: state.actions.concat([action.data])}
-    } else if (action.type === 'LOGIN') {
-        return { ...state, user: action.data.user }
-    }
+	if(action.type === 'ADD_NEW_GROUP') {
+		return { ...state, actions: state.actions.concat([{...action.data, type:'group'}])}
+	} else if (action.type === 'LOGIN') {
+		return { ...state, user: action.data.user }
+	}
 
-    return state
+	return state
 }
 
 export default chatApp
--- test/reducersTest.js
+++ test/reducersTest.js
@@ -1,11 +1,17 @@
-import expect from 'expect'
+import { expect } from 'chai'
+import * as create from '../app/actions/creators.js'
 import reducer from '../app/reducers'
 
-describe('group reducers', () => {
-    xit('should add a new action', ()=>{
-        const action = {type: 'ACTION_CREATED', data: { type: 'GROUP', title: 'test'}}
-        const initialState = {actions:[]}
-        expect(reducer(initialState, action))
-            .toEqual({actions: [{type: 'GROUP', title: 'test'}]})
-    })
+describe('participate reducers', () => {
+	describe('addNewGroup', () => {
+		it('should add a new group', ()=> {
+			const title = 'test'
+			const definition = {}
+			const participants = []
+			const initialState = {actions: []}
+			const finalState = {actions: [{type: 'group', title:title, definition: definition, invitations: []}]}
+			const action = create.newGroupAction(title, definition, participants)
+			expect(reducer(initialState, action)).to.be.eql(finalState)
+		})
+	})
 })
