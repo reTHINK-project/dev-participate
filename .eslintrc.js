ed3288678378debc343521fc16c56a7a8c5f88aa
David Vílchez <dvilchez@xuaps.com>
2017-03-16 11:52:00 +0100

    Fix linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+		"jasmine": true
     },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended","plugin:jasmine/recommended"],
     "parserOptions": {
         "ecmaFeatures": {
             "experimentalObjectRestSpread": true,
@@ -12,7 +13,8 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react",
+		"jasmine"
     ],
     "rules": {
         "indent": [
