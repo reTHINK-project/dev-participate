b53246aa79266c4bfe10cc22f30f48b9be122002
David Vílchez <dvilchez@xuaps.com>
2017-04-28 09:15:42 +0200

    Initialize location obs/rep hyperties

--- app/config/development.config.js
+++ app/config/development.config.js
@@ -2,7 +2,8 @@ const config = {
 	domain: 'localhost',
 	runtimeURL: 'hyperty-catalogue://catalogue.localhost/.well-known/runtime/Runtime',
 	groupChatHyperty: 'hyperty-catalogue://catalogue.localhost/.well-known/hyperty/GroupChat',
-	locationHyperty: 'hyperty-catalogue://catalogue.localhost/.well-known/hyperty/Location',
+	locationReporterHyperty: 'hyperty-catalogue://catalogue.localhost/.well-known/hyperty/LocationReporter',
+	locationObserverHyperty: 'hyperty-catalogue://catalogue.localhost/.well-known/hyperty/LocationObserver',
 	notificationsObserverHyperty: 'hyperty-catalogue://catalogue.localhost/.well-known/hyperty/NotificationsObserver',
 	notificationsReporterHyperty: 'hyperty-catalogue://catalogue.localhost/.well-known/hyperty/NotificationsReporter',
 	discoveryHyperty: 'hyperty-catalogue://catalogue.localhost/.well-known/hyperty/Discovery'
