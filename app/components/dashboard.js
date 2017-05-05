ee08445985056738471359f732692f3eb9535b99
David Vílchez <dvilchez@xuaps.com>
2017-05-05 11:21:57 +0200

    Remove link to admin board from user board

--- app/components/dashboard.js
+++ app/components/dashboard.js
@@ -11,18 +11,7 @@ const Dashboard = ({children}) => {
                             <nav className="navbar navbar-default navbar-fixed-top">
                                 <div className="container-fluid">
                                     <div className="navbar-header">
-                                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                                    <span className="sr-only">Toggle navigation</span>
-                                    <span className="icon-bar"></span>
-                                    <span className="icon-bar"></span>
-                                    <span className="icon-bar"></span>
-                                    </button>    
                                       <Link to="/" className="navbar-brand">Participate</Link>
-                                    </div>
-                                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                                        <ul className="nav navbar-nav">
-                                            <li><Link to="/admin">Admin</Link></li>
-                                        </ul>
                                     </div>
                                 </div>
                             </nav>
