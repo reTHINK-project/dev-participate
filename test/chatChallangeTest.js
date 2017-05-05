4825ba88255c37449ba2c96ad53088286bd5e1e2
David Vílchez <dvilchez@xuaps.com>
2017-05-05 11:11:29 +0200

    Add some missing tests

--- test/chatChallangeTest.js
+++ test/chatChallangeTest.js
@@ -1,9 +1,33 @@
 import { expect } from 'chai'
+import sinon from 'sinon'
 import * as Challenges from '../app/model/challenges'
 
 describe('chat challenge', () => {
 	describe('sendMessage', () => {
-		xit('')
+		const adapter = {
+			sendMessage: sinon.stub()
+		}
+		const message = {text:''}
+		let chat
+
+        before(()=>{
+			chat = Challenges.createChatChallenge(adapter)
+			adapter.sendMessage.resolves(message)
+		})
+
+		it('should delegate the call in the adapter', ()=>{
+			return chat.sendMessage(message)
+				.then(()=>{
+					expect(adapter.sendMessage.calledWith(message)).to.be.true
+				})
+		})
+
+		it('should add the messages to the collection', ()=>{
+			return chat.sendMessage(message)
+				.then((c)=>{
+					expect(c.messages.length).to.be.eql(1)
+				})
+		})
 	})
 
 	describe('newMessageReceived', ()=>{
